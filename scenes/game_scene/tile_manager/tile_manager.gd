4161f0175807394b43cb9e8d52e55564d8cd4c36
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2026-02-16 07:32:53 +1300

    Tile Placements, not working

---
+++ scenes/game_scene/tile_manager/tile_manager.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var tile : PackedScene = preload("res://scenes/game_scene/tile_manager/Tile.tscn")
+
+# TODO: This needs to be in it's level scene
+var grid_size := Vector2(1, 1)
+var tile_size := 64
+var starting_pos := Vector2(128, 128)
+var tile_scale := 0.9
+
+func _ready() -> void:
+	for x in range(0, grid_size.x):
+		for y in range(0, grid_size.y):
+			var instance := tile.instantiate()
+			instance.position = starting_pos * Vector2(x, y)
+			instance.scale = Vector2(tile_size, tile_size)
+			add_child(instance)
+
