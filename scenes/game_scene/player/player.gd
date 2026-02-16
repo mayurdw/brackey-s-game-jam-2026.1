95f2b5fae7488f885ace06a063203d3dc4448eb1
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2026-02-16 19:10:48 +1300

    Diagonal Movement Removed

--- scenes/game_scene/player/player.gd
+++ scenes/game_scene/player/player.gd
@@ -1,18 +1,21 @@
 extends Area2D
 
 # TODO: This needs to come from the level manager
-const MOVEMENT_DISTANCE : int = 64
+const MOVEMENT_DISTANCE : int = 128
 const INPUTS := { "move_up" : Vector2.UP, "move_right" : Vector2.RIGHT, "move_left" : Vector2.LEFT, "move_down" : Vector2.DOWN }
 
-var animation_speed : int = 3
+var animation_speed : float = 0.25
+var is_moving : bool = false
 
 func _input(event: InputEvent) -> void:
 	for key in INPUTS.keys():
-		if event.is_action_pressed(key):
+		if event.is_action_pressed(key) and not is_moving:
 			_move(key)
 
 func _move(key: String) -> void:
+	is_moving = true
 	var tween = create_tween()
 	tween.tween_property(self, "position", 
-		position + INPUTS[key] * MOVEMENT_DISTANCE, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+		position + INPUTS[key] * MOVEMENT_DISTANCE, animation_speed).set_trans(Tween.TRANS_SINE)
 	await tween.finished
+	is_moving = false
