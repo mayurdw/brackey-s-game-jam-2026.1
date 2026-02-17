b75d8930a930363bdc92cfbc4ff5df81704ebc9d
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2026-02-17 19:07:40 +1300

    Player Instantiation inside level

--- scenes/game_scene/levels/new_level.gd
+++ scenes/game_scene/levels/new_level.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var starting_position := Vector2(256, 256)
 @export var manager : PackedScene = load("res://scenes/game_scene/tile_manager/Tile_manager.tscn")
+@export var player : PackedScene = preload("res://scenes/game_scene/player/player.tscn")
 
 @export var levels : Dictionary[int, LevelDetails] = {
 	1 : LevelDetails.new([[0, 0, 2, 0], [0, 1, 1, 0], [0, 0, 1, 1], [1, 1, 1, 0]], 64, 16)
@@ -9,7 +10,13 @@ extends Node2D
 
 func _ready() -> void:
 	var tile_manager = manager.instantiate()
+	var p = player.instantiate()
+
 	tile_manager.position = Vector2.ZERO
+	p.position = Vector2.ZERO
+	# TODO: Figure out how to set this up
+	p.set_starting_position(levels[1].gap, Vector2(4, 4), Vector2(levels[1].gap, levels[1].gap))
 	tile_manager.level = levels[1]
 	add_child(tile_manager)
+	add_child(p)
 	tile_manager.populate()
--- scenes/game_scene/player/player.gd
+++ scenes/game_scene/player/player.gd
@@ -1,25 +1,34 @@
 extends Area2D
 
 # TODO: This needs to come from the level manager
-const MOVEMENT_DISTANCE : int = 80
+var movement_distance : int = 80
 const INPUTS := { "move_up" : Vector2.UP, "move_right" : Vector2.RIGHT, "move_left" : Vector2.LEFT, "move_down" : Vector2.DOWN }
 
 var animation_speed : float = 0.25
 var is_moving : bool = false
-var centre : Vector2 = Vector2(MOVEMENT_DISTANCE, MOVEMENT_DISTANCE)
+var centre : Vector2 = Vector2(movement_distance, movement_distance)
 
-func _ready() -> void:
-	position = centre + Vector2(MOVEMENT_DISTANCE, 0)
+func set_starting_position(movement_distance: int, grid_size: Vector2, centre: Vector2) -> void:
+	centre = centre
+	position = centre + movement_distance * Vector2.RIGHT
 
 func _input(event: InputEvent) -> void:
 	for key in INPUTS.keys():
 		if event.is_action_pressed(key) and not is_moving:
 			_move(key)
+			return
+	
+	if event.is_action_pressed("interact"):
+		_move_centre()
+
+func _move_centre() -> void:
+	centre = position
+	position = centre + movement_distance * Vector2.RIGHT
 
 func _move(key: String) -> void:
 	is_moving = true
 	var tween = create_tween()
 	tween.tween_property(self, "position", 
-		centre + INPUTS[key] * MOVEMENT_DISTANCE, animation_speed).set_trans(Tween.TRANS_SINE)
+		centre + INPUTS[key] * movement_distance, animation_speed).set_trans(Tween.TRANS_SINE)
 	await tween.finished
 	is_moving = false
