1a8a85049dc74ff443908d09e52f14d1529659ea
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2026-02-17 19:17:31 +1300

    Player Starting Position

--- scenes/game_scene/player/player.gd
+++ scenes/game_scene/player/player.gd
@@ -2,26 +2,34 @@ extends Area2D
 
 # TODO: This needs to come from the level manager
 var movement_distance : int = 80
-const INPUTS := { "move_up" : Vector2.UP, "move_right" : Vector2.RIGHT, "move_left" : Vector2.LEFT, "move_down" : Vector2.DOWN }
+const INPUTS : Dictionary[String, Vector2]= { "move_up" : Vector2.UP, "move_right" : Vector2.RIGHT, "move_left" : Vector2.LEFT, "move_down" : Vector2.DOWN }
+const UNMOVED_INPUTS : Dictionary[String, Vector2]= { "move_up" : Vector2.UP, "move_down" : Vector2.DOWN }
 
 var animation_speed : float = 0.25
 var is_moving : bool = false
 var centre : Vector2 = Vector2(movement_distance, movement_distance)
+var has_moved: bool = false
 
 func set_starting_position(movement_distance: int, grid_size: Vector2, centre: Vector2) -> void:
+	has_moved = false
 	centre = centre
-	position = centre + movement_distance * Vector2.RIGHT
+	position = centre
 
-func _input(event: InputEvent) -> void:
-	for key in INPUTS.keys():
+func handle_movement_key(event: InputEvent, keys: Dictionary[String, Vector2]) -> void:
+	for key in keys.keys():
 		if event.is_action_pressed(key) and not is_moving:
 			_move(key)
 			return
-	
+
+func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact"):
 		_move_centre()
+		return
+
+	handle_movement_key(event, INPUTS if has_moved else UNMOVED_INPUTS)
 
 func _move_centre() -> void:
+	has_moved = true
 	centre = position
 	position = centre + movement_distance * Vector2.RIGHT
 
