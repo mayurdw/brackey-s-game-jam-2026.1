1d01aa6e56e8a5662e0c173d44a894d9ed07a02d
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2026-02-17 21:32:07 +1300

    Movement Limitatations, needs a bit of redo

--- scenes/game_scene/player/player.gd
+++ scenes/game_scene/player/player.gd
@@ -9,16 +9,51 @@ var animation_speed : float = 0.25
 var is_moving : bool = false
 var centre : Vector2 = Vector2(movement_distance, movement_distance)
 var has_moved: bool = false
+var bounds : Vector2 = Vector2.ZERO
+var current_position_in_grid : Vector2 = Vector2.ZERO
+var centre_position_in_grid : Vector2 = Vector2.ZERO
 
 func set_starting_position(movement_distance: int, grid_size: Vector2, centre: Vector2) -> void:
 	has_moved = false
 	centre = centre
 	position = centre
+	bounds = grid_size
+
+func is_movement_possible(key: Vector2) -> bool:
+	if has_moved:
+		match (key):
+			Vector2.UP: return current_position_in_grid.y > 0
+			Vector2.DOWN: return current_position_in_grid.y < bounds.y - 1
+			Vector2.RIGHT: return (centre_position_in_grid.x >= current_position_in_grid.x and current_position_in_grid.x < bounds.x - 1)
+			Vector2.LEFT: return current_position_in_grid.x > 0
+			_: return false
+	else:
+		match (key):
+			Vector2.UP: return current_position_in_grid.y > 0
+			Vector2.DOWN: return current_position_in_grid.y < bounds.y - 1
+			_: return false
+
+func move_position(key: Vector2) -> void:
+	match (key):
+		Vector2.UP: current_position_in_grid.y -= 1
+		Vector2.DOWN: current_position_in_grid.y += 1
+		Vector2.RIGHT: current_position_in_grid.x += 1
+		Vector2.LEFT: current_position_in_grid.x -= 1
 
 func handle_movement_key(event: InputEvent, keys: Dictionary[String, Vector2]) -> void:
 	for key in keys.keys():
-		if event.is_action_pressed(key) and not is_moving:
-			_move(key)
+		if not is_moving and event.is_action_pressed(key) and is_movement_possible(keys[key]):
+			is_moving = true
+			var tween = create_tween()
+			move_position(keys[key])
+			
+			if has_moved:
+				tween.tween_property(self, "position", centre + keys[key] * movement_distance, animation_speed).set_trans(Tween.TRANS_SINE)
+			else:
+				tween.tween_property(self, "position", position + keys[key] * movement_distance, animation_speed).set_trans(Tween.TRANS_SINE)
+
+			await tween.finished
+			is_moving = false
 			return
 
 func _input(event: InputEvent) -> void:
@@ -26,17 +61,13 @@ func _input(event: InputEvent) -> void:
 		_move_centre()
 		return
 
-	handle_movement_key(event, INPUTS if has_moved else UNMOVED_INPUTS)
+	if has_moved:
+		handle_movement_key(event, INPUTS)
+	else:
+		handle_movement_key(event, UNMOVED_INPUTS)
 
 func _move_centre() -> void:
 	has_moved = true
 	centre = position
+	centre_position_in_grid = current_position_in_grid
 	position = centre + movement_distance * Vector2.RIGHT
-
-func _move(key: String) -> void:
-	is_moving = true
-	var tween = create_tween()
-	tween.tween_property(self, "position", 
-		centre + INPUTS[key] * movement_distance, animation_speed).set_trans(Tween.TRANS_SINE)
-	await tween.finished
-	is_moving = false
