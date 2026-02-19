6b2714a517416defa712855d638eb3dbb615857b
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2026-02-19 14:45:13 +1300

    Tutorial Pauses Game

--- scenes/game_scene/levels/new_level.gd
+++ scenes/game_scene/levels/new_level.gd
@@ -3,20 +3,36 @@ extends Node2D
 @export var starting_position := Vector2(256, 256)
 @export var manager : PackedScene = load("res://scenes/game_scene/tile_manager/Tile_manager.tscn")
 @export var player : PackedScene = preload("res://scenes/game_scene/player/player.tscn")
-@export var levels : Dictionary[int, LevelDetails] = {
-	1 : LevelDetails.new([
+var levels : Dictionary[int, LevelDetails] = {
+	0 : LevelDetails.new([
 		[1, 0, 1, 0],
 		[1, 1, 1, 0],
 		[1, 0, 1, 1],
 		[1, 1, 1, 0]],
 		80,
+		16),
+	1 : LevelDetails.new([
+		[0, 1, 1, 0, 1, 1, 1, 0],
+		[1, 1, 0, 1, 0, 0, 1, 0],
+		[0, 1, 1, 0, 1, 0, 0, 0],
+		[0, 1, 1, 1, 1, 0, 0, 0],
+		[0, 0, 1, 1, 0, 1, 0, 1],
+		[1, 0, 0, 1, 1, 1, 1, 0],
+		[1, 0, 1, 1, 1, 0, 0, 0],
+		[1, 1, 0, 0, 1, 1, 1, 0],
+		[0, 1, 0, 0, 1, 0, 1, 1]
+		],
+		80,
 		16)
 }
+@export var level: int
 
 var tile_manager : TileManager
 var p : Player
 var connections : Array[Vector2]
 
+signal level_lost
+signal level_won
 
 func _ready() -> void:
 	tile_manager = manager.instantiate()
@@ -24,9 +40,12 @@ func _ready() -> void:
 
 	tile_manager.position = Vector2.ZERO
 	p.position = Vector2.ZERO
-	# TODO: Figure out how to set this up
-	p.set_starting_position(levels[1].gap, Vector2(4, 4), Vector2(levels[1].gap, levels[1].gap))
-	tile_manager.level = levels[1]
+	p.set_starting_position(
+		levels[level].gap, 
+		Vector2(levels[level].grid_size.size(), levels[level].grid_size[0].size()), 
+		Vector2(levels[level].gap, levels[level].gap)
+	)
+	tile_manager.level = levels[level]
 	add_child(tile_manager)
 	add_child(p)
 
@@ -36,8 +55,8 @@ func _ready() -> void:
 	tile_manager.populate()
 
 func _game_over() -> void:
-	print("On unsafe tile, game over")
 	p.controls_enabled = false
+	level_lost.emit()
 
 func _new_center_position(centre_position: Vector2) -> void:
 	connections.append(centre_position)
@@ -45,3 +64,4 @@ func _new_center_position(centre_position: Vector2) -> void:
 
 func _level_completed() -> void:
 	p.controls_enabled = false
+	level_won.emit()
